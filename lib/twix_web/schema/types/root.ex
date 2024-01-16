c87f64364a598121f67fe3e1d94cafc32d7607ee
Raul Pereira <raul.pereira@gmail.com>
2024-01-16 17:32:44 -0300

    create, retrieve, and update user

---
+++ lib/twix_web/schema/types/root.ex
@@ -0,0 +1,29 @@
+defmodule TwixWeb.Schema.Types.Root do
+  use Absinthe.Schema.Notation
+
+  alias TwixWeb.Resolvers.User, as: UserResolver
+
+  import_types TwixWeb.Schema.Types.User
+
+  object :root_query do
+    field :user, type: :user do
+      arg :id, non_null(:id)
+
+      resolve &UserResolver.get/2
+    end
+  end
+
+  object :root_mutation do
+    field :create_user, type: :user do
+      arg :input, non_null(:create_user_input)
+
+      resolve &UserResolver.create/2
+    end
+
+    field :update_user, type: :user do
+      arg :input, non_null(:update_user_input)
+
+      resolve &UserResolver.update/2
+    end
+  end
+end
