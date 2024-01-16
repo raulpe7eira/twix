076186eeb7fdae4218af350945302a8d8f687e4f
Raul Pereira <raul.pereira@gmail.com>
2024-01-16 17:41:30 -0300

    handle ecto errors w/ crudry

--- lib/twix_web/schema/types/root.ex
+++ lib/twix_web/schema/types/root.ex
@@ -1,6 +1,7 @@
 defmodule TwixWeb.Schema.Types.Root do
   use Absinthe.Schema.Notation
 
+  alias Crudry.Middlewares.TranslateErrors
   alias TwixWeb.Resolvers.User, as: UserResolver
 
   import_types TwixWeb.Schema.Types.User
@@ -18,12 +19,14 @@ defmodule TwixWeb.Schema.Types.Root do
       arg :input, non_null(:create_user_input)
 
       resolve &UserResolver.create/2
+      middleware TranslateErrors
     end
 
     field :update_user, type: :user do
       arg :input, non_null(:update_user_input)
 
       resolve &UserResolver.update/2
+      middleware TranslateErrors
     end
   end
 end
