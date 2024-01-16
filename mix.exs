7e44ffda2368f87a89b1555e1ffc852d4399246b
Raul Pereira <raul.pereira@gmail.com>
2024-01-16 17:17:41 -0300

    setup graphql

--- mix.exs
+++ mix.exs
@@ -41,7 +41,9 @@ defmodule Twix.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:absinthe, "~> 1.7"},
+      {:absinthe_plug, "~> 1.5"}
     ]
   end
 
