1d44643cf287b3310c80a5154a933d1069bbaeb4
Raul Pereira <raul.pereira@gmail.com>
2024-01-16 17:53:11 -0300

    handle ecto errors w/ absinthe_error_payload

--- lib/twix_web/schema/types/root.ex
+++ lib/twix_web/schema/types/root.ex
@@ -1,7 +1,9 @@
 defmodule TwixWeb.Schema.Types.Root do
   use Absinthe.Schema.Notation
 
-  alias Crudry.Middlewares.TranslateErrors
+  import AbsintheErrorPayload.Payload
+
+  # alias Crudry.Middlewares.TranslateErrors
   alias TwixWeb.Resolvers.User, as: UserResolver
 
   import_types TwixWeb.Schema.Types.User
@@ -15,18 +17,19 @@ defmodule TwixWeb.Schema.Types.Root do
   end
 
   object :root_mutation do
-    field :create_user, type: :user do
+    field :create_user, type: :user_payload do
       arg :input, non_null(:create_user_input)
 
       resolve &UserResolver.create/2
-      middleware TranslateErrors
+      middleware &build_payload/2
+      # middleware TranslateErrors
     end
 
     field :update_user, type: :user do
       arg :input, non_null(:update_user_input)
 
       resolve &UserResolver.update/2
-      middleware TranslateErrors
+      # middleware TranslateErrors
     end
   end
 end
